62ace57889e3c393160d0fd029f9c5ef2df99903
Abileshjr <167401947+Abileshjr@users.noreply.github.com>
2025-02-26 17:07:57 +0530

    Update loogin.py

--- loogin.py
+++ loogin.py
@@ -1,12 +1,10 @@
 import streamlit as st
-# Set page title and favicon
-st.set_page_config(page_title="Login Page", page_icon="🔐", layout="centered")
-st.image(""C:\Users\kidjiraiya\Desktop\anime-one-piece-brook-one-piece-franky-one-piece-wallpaper-preview.jpg"", use_container_width=True)
-# Centered Login Form
+st0.set_page_config(page_title="Login Page", page_icon="🔐", layout="centered")
+st.image("C:\Users\kidjiraiya\Desktop\anime-one-piece-brook-one-piece-franky-one-piece-wallpaper-preview.jpg", use_container_width=True)
+# for Centered Login Form
 st.markdown("<h1>PIRATES RECORDS</h1>", unsafe_allow_html=True)
 st.write("")
-
-# Login Form
+# for Login Form
 with st.form("login_form", clear_on_submit=True):
     username = st.text_input("Username", placeholder="Enter your PIRATE NAME")
     password = st.text_input("PI-code", type="password", placeholder="Enter your SECRET CODE")
@@ -16,8 +14,7 @@ with st.form("login_form", clear_on_submit=True):
         submit = st.form_submit_button("Submit")
     with col2:
         cancel = st.form_submit_button("Cancel")
-
-# Handle Button Clicks
+# for Button Clicks
 if submit:
     if username and password:
         st.success(f"Login successful! Welcome, {username}!")
