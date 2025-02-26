df39afdda4b124970f75389e6f87dc4ff54c91fb
Abileshjr <167401947+Abileshjr@users.noreply.github.com>
2025-02-26 17:32:37 +0530

    Update loogin.py

--- loogin.py
+++ loogin.py
@@ -4,7 +4,7 @@ st.markdown(
     """
     <style>
     [data-testid="stAppViewContainer"] {
-        background-image: url("https://editorial.pxcrush.net/carsales/general/editorial/2024-ford-mustang-gt-fastback-manual-05.jpg?width=1024&height=682");
+        background-image: url("https://c4.wallpaperflare.com/wallpaper/965/883/624/manga-one-piece-wallpaper-preview.jpg");
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
